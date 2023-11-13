fa450ded54c3390c71c030e8015f7c39af287521
Bowen 404 <ali12333@126.com>
2023-11-13 22:31:22 +0800

    Update index.ts
    prettier

--- index.ts
+++ index.ts
@@ -2,14 +2,7 @@ import {Context, ValidationError} from 'hydrooj';
 
 export function apply (ctx: Context) {
     ctx.on ('handler/before/HomeAvatar', (h) => {
-            try {
-                if (h.request.files.file) { throw new ValidationError (' 不允许上传头像 ');  }
-            } catch (e) {
-                if (e.name=='TypeError') {console.error(e.name);} //h.request.files.file会因file undefined产生TypeError导致qq无法使用
-                else if (e.name == 'ValidationError') {throw new ValidationError (' 不允许上传头像233 ');}
-            }
-            ;
-
+            if (h.request.files?.file) throw new ValidationError (' 不允许上传头像 ');
         }
     );
 }
